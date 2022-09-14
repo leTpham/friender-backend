ec1f944d00b8962c0179030a7faa1dd7af92751f
Billy Russell <billy.hrussell@gmail.com>
2022-09-13 17:16:41 -0700

    routes skeleton

---
+++ app.py
@@ -0,0 +1,67 @@
+import os
+# from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, flash, redirect, session, g
+
+from sqlalchemy.exc import IntegrityError
+
+from models import db, connect_db, User, Message, Like
+
+# load_dotenv()
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+connect_db(app)
+
+##############################################################################
+# User signup/login/logout
+
+@app.before_request # global user
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+# TODO: How to get form info from JS to python?
+@app.route('/signup', methods="POST")
+
+
+@app.route('/login', method="POST")
+
+
+@app.route('/logout', method="POST")
+
+##############################################################################
+# General user routes: IF LOGGED IN
+
+# Show ALL users
+@app.get('/users')
+
+# Show one user
+@app.get('/users/<username>')
+
+
+##############################################################################
+# Messages routes:
+
+# Show all messages created by currUser & sent to currUser
+@app.get('/messages')
+
+
+# Create a new message
+@app.route('/messages/<username>', methods=["GET","POST"])
---
+++ models.py
@@ -0,0 +1,159 @@
+
+from datetime import datetime
+
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    username = db.Column(
+        db.text,
+        primary_key=True,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    name = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    hobbies = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    interests = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    zipcode = db.Column(
+        db.Integer,
+    )
+
+    radius = db.Column(
+        db.Integer,
+    )
+
+    image = db.Column(
+        db.Text,
+    )
+
+    messages = db.relationship('Message', backref="user")
+    # TODO: relationship b/t swiped & user 
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}, {self.email}>"
+
+    @classmethod
+    def signup(cls, username, password, name, hobbies, interests, zipcode, radius, image):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            password=hashed_pwd,
+            name=name,
+            hobbies=hobbies,
+            interests=interests,
+            zipcode=zipcode,
+            radius=radius,
+            image=image
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If this can't find matching user (or if password is wrong), returns
+        False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+
+class Message(db.Model):
+
+    __tablename__ = "messages"
+
+    sender = db.Column(
+        db.Text,
+        db.ForeignKey("users.id"),
+        primary_key=True,
+    )
+    reciever = db.Column(
+        db.Text,
+        db.ForeignKey("users.id"),
+        primary_key=True,
+    )
+    msg = db.Column(
+        db.Text
+    )
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+    )
+
+#TODO: Set method to compare whether both true,
+class Swiped(db.Model):
+
+    __tablename__ = "swiped"
+
+    u1 = db.Column(
+        db.Text,
+        db.ForeignKey("users.id"),
+        primary_key=True,
+    )
+    u2 = db.Column(
+        db.Text,
+        db.ForeignKey("users.id"),
+        primary_key=True,
+    )
+    u1_swiped = db.Column(
+        db.Boolean,
+        nullable=True
+    )
+    u2_swiped = db.Column(
+        db.Boolean,
+        nullable=True
+    )
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
